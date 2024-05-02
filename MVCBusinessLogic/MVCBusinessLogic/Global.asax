939db1fa080260651348887028f9691d17e409bd
Sahil Dharaviya <123103366+Sahil18072003@users.noreply.github.com>
2024-05-02 15:15:14 +0530

    MVC
    MVC

---
+++ MVCBusinessLogic/MVCBusinessLogic/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCBusinessLogic.MvcApplication" Language="C#" %>
---
+++ MVCBusinessLogic/MVCNusinessLogic.DbConnection/Program.cs
@@ -0,0 +1,39 @@
+﻿using MVCBusinessLogic.library;  
+using System;  
+using System.Collections.Generic;  
+using System.Configuration;  
+using System.Data;  
+using System.Data.SqlClient;  
+namespace MVCNusinessLogic.DbConnection
+{
+    public class DL_Employee
+    {
+        public IEnumerable<BE_Employee> Employees
+        {
+            get
+            {
+                string connectionString = ConfigurationManager.ConnectionStrings["conStr"].ConnectionString;
+                List<BE_Employee> employees = new List<BE_Employee>();
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    SqlCommand cmd = new SqlCommand("USP_GetAllEmployees", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    //cmd.Parameters.AddWithValue("@DeptId", null);   
+                    con.Open();
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                    while (rdr.Read())
+                    {
+                        BE_Employee employee = new BE_Employee();
+                        employee.ID = Convert.ToInt32(rdr["Id"]);
+                        employee.Name = rdr["Name"].ToString();
+                        employee.Gender = rdr["Gender"].ToString();
+                        employee.Salary = Convert.ToInt32(rdr["Salary"]);
+                        employee.DeptId = Convert.ToInt32(rdr["DeptId"]);
+                        employees.Add(employee);
+                    }
+                }
+                return employees;
+            }
+        }
+    }
+}
